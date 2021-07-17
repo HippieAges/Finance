15be929269f41190d153659dae599407a7b2eda0
HippieAges <OmelchenkoOleksiy@gmail.com>
2021-07-16 22:17:59 -0700

    switch from flexbox only to flexbox + grid

--- calendar/script.js
+++ calendar/script.js
@@ -7,12 +7,51 @@ const rows = 6
 const days = ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"]
 
 const currentDate = new Date();
-const [month, year, day] = [months[currentDate.getMonth()], currentDate.getFullYear(), currentDate.getDay()];
+const currentDateMonth = currentDate.getMonth();
+const [month, year, day] = [months[currentDateMonth], currentDate.getFullYear(), currentDate.getDate()];
 
-document.getElementById("current-date").innerText = `${month} ${day} ${year}`
+document.getElementById("date").innerText = `${month} ${year}`
 
-let displayedDays = new Date(year, month)
-displayedDays = displayedDays.getDay() === 1 ? displayedDays : new Date(year, month, displayedDays.getDate() - (7 - displayedDays.getDay()));
-for(let count = 0; count < rows * days.length; count++) {
-    document.querySelector('#all-days').innerHTML
+let displayedDays = new Date(year, currentDateMonth);
+console.log(displayedDays);
+displayedDays = displayedDays.getDay() === 1 ? displayedDays : new Date(year, currentDateMonth, displayedDays.getDate() - (7 - displayedDays.getDay()));
+console.log(displayedDays);
+let date = "";
+let count = 0;
+
+// applies to the days in the previous month //
+while (displayedDays.getMonth() !== currentDateMonth) {
+    if(count === 0) {
+        date += `<div id="row">`
+    } else if(count % 7 === 0) {
+        date += `</div><div id="row">`
+    }
+    date += `<div id="prior-days">${displayedDays.getDate()}</div>`; 
+    displayedDays.setDate(displayedDays.getDate() + 1);
+    count++;
+}
+
+// applies to the days in the current month //
+for(;displayedDays.getMonth() === currentDateMonth; count++) {
+    if(count % 7 === 0) {
+        date += `</div><div id="row">`
+    }
+    if (displayedDays.getDate() === day) {
+        date += `<div id="today">${displayedDays.getDate()}</div>`;
+    } else {
+        date += `<div>${displayedDays.getDate()}</div>`;
+    }
+    displayedDays.setDate(displayedDays.getDate() + 1);
+}
+
+// applies to the days in the next month //
+for(;count < rows * days.length; count++) {
+    if(count == rows * days.length) {
+        date += `</div>`
+    } else if(count % 7 === 0) {
+        date += `</div><div id="row">`
+    }
+    date += `<div id="next-days">${displayedDays.getDate()}</div>`;
+    displayedDays.setDate(displayedDays.getDate() + 1);
+    document.querySelector('#day').innerHTML = date;
 }
